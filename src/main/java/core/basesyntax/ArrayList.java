1e98ac2bfbae1a1cb12529fb3ff50fe2dcbddad7
Nataliia <gzodchie@gmail.com>
2023-10-11 17:07:30 +0300

    for fixed errors

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,31 +1,83 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+    private T value;
+    private Object[] elements;
+    private int index;
+    private static final int INITIAL_CAPACITY = 10;
+    private int capacity;
+    private int numberOfElements;
 
+    public ArrayList() {
+        elements = new Object[INITIAL_CAPACITY];
+        capacity = INITIAL_CAPACITY;
     }
 
+    // У методі add(E element) перед додаванням кожного нового елемента
+    // перевіряється capacity (місткість) масиву.
+    // Якщо поточна кількість елементів (разом з новим елементом) перевищує довжину elementData,
+    // то створюється новий масив, місткість якого більша в 1,5 рази порівняно зі старим масивом,
+    // а потім всі елементи зі старого масиву копіюються в новий.
+    // Перевіряємо, що індекс позиції менше розміру масиву. Вставляємо.
+    @Override
+    public void add(T value) {
+        checkCapacity(capacity);
+
+        elements[numberOfElements] = value;
+        numberOfElements++;
+    }
+
+    private void checkCapacity(int capacity) {
+        if (numberOfElements > capacity) {
+            int newCapacity = elements.length + elements.length >> 1;
+            Object[] newElements = new Object[newCapacity];
+            System.arraycopy(elements, 0, newElements, 0, numberOfElements);
+            elements = newElements;
+        }
+    }
+
+    private void rangeIndexCheck(int index) {
+        if (index >= numberOfElements || index < 0)
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " outside array");
+    }
+
+    // Коли новий елемент вставляється на вказану позицію i,
+    // усі елементи в масиві elementData, починаючи з позиції i, зміщуються праворуч.
+    // System.arrayCopy()
+    // використовується метод System.arrayCopy(),
+    // який копіює старі значення на нові позиції.
     @Override
     public void add(T value, int index) {
-
+        rangeIndexCheck(index);
+        checkCapacity(capacity + 1);
+        for (int i = numberOfElements; i > index; i--) {
+            elements[i] = elements[i-1];
+        }
+        elements[index] = value;
+        numberOfElements++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        checkCapacity(numberOfElements + list.size());
+        System.arraycopy(list, 0, elements, numberOfElements, list.size());
     }
 
     @Override
     public T get(int index) {
-        return null;
+        rangeIndexCheck(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        rangeIndexCheck(index);
+        elements[index] = value;
     }
 
+    // коли елемент видаляється, всі елементи зсуваються ліворуч.
+    // використовується метод System.arrayCopy(),
+    // який копіює старі значення на нові позиції.
     @Override
     public T remove(int index) {
         return null;
@@ -38,11 +90,11 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return numberOfElements;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return numberOfElements == 0;
     }
 }
